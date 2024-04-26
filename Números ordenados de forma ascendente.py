406965d76f1f0a5a565709b86e249907af5ee22d
MauricioCastroL <168048958+MauricioCastroL@users.noreply.github.com>
2024-04-25 21:57:48 -0400

    Add files via upload

---
+++ Números ordenados de forma ascendente.py
@@ -0,0 +1,7 @@
+valor = float(input("¿Cual es nuestro primer valor?: "))
+valor1 = float(input("¿Cuál es nuestro segundo valor?: "))
+if valor<valor1:
+    print("Nuestros valores ordenados de forma ascendente son: ", valor, valor1)
+if valor>valor1:
+    print("Nuestros valores ordenados de forma ascendenre son: ", valor1, valor)
+    
